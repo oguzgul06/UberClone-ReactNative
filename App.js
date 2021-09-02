72d14d9c1f28945947c08360c5ec66a1b4eb896c
Oğuz GÜL <ogzgl06@gmail.com>
2021-09-02 21:31:57 +0300

    Implemanting Redux

--- App.js
+++ App.js
@@ -1,22 +1,25 @@
-import React from 'react'
-import { StatusBar, StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {Provider} from 'react-redux';
+import { store } from './component/store';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Let's Build Uber Clone with React Native 🚀</Text>
-      <StatusBar style="auto"/>
-    </View>
-  )
-}
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Let's Build Uber Clone with React Native 🚀</Text>
+      </View>
+    </Provider>
+  );
+};
 
-export default App
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: "center",
-    justifyContent: "center",
-  }
-})
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
