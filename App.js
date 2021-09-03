8dbed4f4758a1b2db4d850dfc46f2655b2f245d7
Oğuz GÜL <ogzgl06@gmail.com>
2021-09-03 12:10:31 +0300

    Implementing the ReactNative Navigation

--- App.js
+++ App.js
@@ -3,14 +3,38 @@ import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
 import { store } from "./components/store";
 import HomeScreen from "./screens/HomeScreen";
+import MapScreen from "./screens/MapScreen";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+/* import "react-native-safe-gesture-handler"; */
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const App = () => {
+  const Stack = createNativeStackNavigator();
+
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <HomeScreen />
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+
+            <Stack.Screen
+              name="MapScreen"
+              component={MapScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 };
